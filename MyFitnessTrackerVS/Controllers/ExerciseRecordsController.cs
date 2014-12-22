2052a1f57118409475d474c11cd87985f2d195c7
lionadi <adrian.simionescu82@gmail.com>
2014-12-22 18:12:29 +0200

    New Version 3

---
+++ MyFitnessTrackerVS/Controllers/ExerciseRecordsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyFitnessTrackerVS;
+
+namespace MyFitnessTrackerVS.Controllers
+{
+    public class ExerciseRecordsController : Controller
+    {
+        private MyFitnessTrackerDBEntities db = new MyFitnessTrackerDBEntities();
+
+        // GET: ExerciseRecords
+        public ActionResult Index()
+        {
+            var exerciseRecords = db.ExerciseRecords.Include(e => e.Exercise);
+            return View(exerciseRecords.ToList());
+        }
+
+        // GET: ExerciseRecords/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ExerciseRecord exerciseRecord = db.ExerciseRecords.Find(id);
+            if (exerciseRecord == null)
+            {
+                return HttpNotFound();
+            }
+            return View(exerciseRecord);
+        }
+
+        // GET: ExerciseRecords/Create
+        public ActionResult Create()
+        {
+            ViewBag.ExerciseId = new SelectList(db.Exercises, "Id", "Name");
+            return View();
+        }
+
+        // POST: ExerciseRecords/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Record,Date,StartDate,EndDate,ExerciseId")] ExerciseRecord exerciseRecord)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ExerciseRecords.Add(exerciseRecord);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ExerciseId = new SelectList(db.Exercises, "Id", "Name", exerciseRecord.ExerciseId);
+            return View(exerciseRecord);
+        }
+
+        // GET: ExerciseRecords/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ExerciseRecord exerciseRecord = db.ExerciseRecords.Find(id);
+            if (exerciseRecord == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ExerciseId = new SelectList(db.Exercises, "Id", "Name", exerciseRecord.ExerciseId);
+            return View(exerciseRecord);
+        }
+
+        // POST: ExerciseRecords/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Record,Date,StartDate,EndDate,ExerciseId")] ExerciseRecord exerciseRecord)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(exerciseRecord).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ExerciseId = new SelectList(db.Exercises, "Id", "Name", exerciseRecord.ExerciseId);
+            return View(exerciseRecord);
+        }
+
+        // GET: ExerciseRecords/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ExerciseRecord exerciseRecord = db.ExerciseRecords.Find(id);
+            if (exerciseRecord == null)
+            {
+                return HttpNotFound();
+            }
+            return View(exerciseRecord);
+        }
+
+        // POST: ExerciseRecords/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            ExerciseRecord exerciseRecord = db.ExerciseRecords.Find(id);
+            db.ExerciseRecords.Remove(exerciseRecord);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MyFitnessTrackerVS/Controllers/ExercisesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyFitnessTrackerVS;
+
+namespace MyFitnessTrackerVS.Controllers
+{
+    public class ExercisesController : Controller
+    {
+        private MyFitnessTrackerDBEntities db = new MyFitnessTrackerDBEntities();
+
+        // GET: Exercises
+        public ActionResult Index()
+        {
+            return View(db.Exercises.ToList());
+        }
+
+        // GET: Exercises/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Exercise exercise = db.Exercises.Find(id);
+            if (exercise == null)
+            {
+                return HttpNotFound();
+            }
+            return View(exercise);
+        }
+
+        // GET: Exercises/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Exercises/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Target,UserID")] Exercise exercise)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Exercises.Add(exercise);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(exercise);
+        }
+
+        // GET: Exercises/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Exercise exercise = db.Exercises.Find(id);
+            if (exercise == null)
+            {
+                return HttpNotFound();
+            }
+            return View(exercise);
+        }
+
+        // POST: Exercises/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Target,UserID")] Exercise exercise)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(exercise).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(exercise);
+        }
+
+        // GET: Exercises/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Exercise exercise = db.Exercises.Find(id);
+            if (exercise == null)
+            {
+                return HttpNotFound();
+            }
+            return View(exercise);
+        }
+
+        // POST: Exercises/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Exercise exercise = db.Exercises.Find(id);
+            db.Exercises.Remove(exercise);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
